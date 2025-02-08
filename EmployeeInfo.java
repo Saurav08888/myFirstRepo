92aa2877f03ffe6ebb28414784f06777b7c9c751
Saurav <Sauravjaat2020@gmail.com>
2025-02-08 17:45:57 +0530

    Add files via upload

---
+++ EmployeeInfo.java
@@ -0,0 +1,11 @@
+public class EmployeeInfo {
+
+    public static void main(String[] args) {
+        System.out.println("NAME\tMAIL-ID\t\tEMPLOYEE-CODE\tPHONE");
+        System.out.println("Saurav\tsaurav@cse\tp0987\t\t9988787877");
+        System.out.println("Komal\tkomal@cse\tp0986\t\t998878745664");
+        System.out.println("Jyoti\tjyoti@cse\tp0956\t\t998878356646");
+        System.out.println("Nikita\tnikita@cse\tp0966\t\t9988787667");
+        System.out.println("Sakshi\tsakshi@cse\tp0967\t\t9988787555");
+    }
+}
