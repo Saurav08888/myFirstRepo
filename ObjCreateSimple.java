d9e38825002f706920decd3ac6def0a0e722c1bf
Saurav <Sauravjaat2020@gmail.com>
2025-02-10 00:13:02 +0530

    Add files via upload

---
+++ ObjCreateSimple.java
@@ -0,0 +1,13 @@
+public class ObjCreateSimple {
+    int age;
+    String name;
+
+    public static void main(String[] args) {
+        ObjCreateSimple stu=new ObjCreateSimple();
+        stu.age=22;
+        stu.name="Saurav";
+        System.out.println("Name: "+stu.name);
+        System.out.println("Age: "+stu.age);
+        
+    }
+}
