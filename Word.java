2951bb2475ccf285e345c3a6cc64c70ee92a644d
Saurav <Sauravjaat2020@gmail.com>
2025-02-21 11:59:39 +0530

    Add files via upload

---
+++ Word.java
@@ -0,0 +1,39 @@
+/*
+ * int a_number=1; (range is 1 to 5 both including)
+Print the value in a number in word with and without using equality (==) operator. For example it should print "Four" if a_number contains 4.
+ */
+public class Word {
+    public static void main(String[] args) {
+        
+    
+
+    int number=5;
+
+    if(number==1)
+    {
+        System.out.println("ONE");
+    }
+    if(number==2)
+    {
+        System.out.println("TWO");
+
+    }
+    if(number==3)
+    {
+        System.out.println("THREE");
+    }
+    if(number==4)
+
+    {
+        System.out.println("FOUR");
+
+    }
+    if(number==5)
+    {
+        System.out.println("FIVE");
+    }
+
+    }
+
+    
+}
