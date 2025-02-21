4249fe782c2be97f16b91424726b4dd491b8798d
Saurav <Sauravjaat2020@gmail.com>
2025-02-21 12:16:28 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,10 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] A={3,6,4,7,8};
+        int n=A.length;
+        for(int i=0;i<n;i++)
+        {
+        System.out.println("The Array is: "+A[i]+" ");
+        }
+    }
+}
